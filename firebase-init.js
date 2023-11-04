d38b4f881254b28669433b692e0f669ba9acab4f
José Alan <104922063+Alanz0ka@users.noreply.github.com>
2023-11-04 14:15:21 -0300

    Separando a chave de api

---
+++ firebase-init.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA6nIPvtLPwg21cxgtSPBUBGuaAg3Va4kQ",
+    authDomain: "controle-de-gastos-2fc38.firebaseapp.com",
+    projectId: "controle-de-gastos-2fc38",
+    storageBucket: "controle-de-gastos-2fc38.appspot.com",
+    messagingSenderId: "222413456303",
+    appId: "1:222413456303:web:76e2731d5e295a110ee55a",
+    measurementId: "G-C3L5WZFHBX"
+  };
+  firebase.initializeApp(firebaseConfig)
